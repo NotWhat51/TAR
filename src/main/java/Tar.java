5fc595d3120bb86341da62e14a28c54769dd3462
notwhat <belskaya.se@edu.spbstu.ru>
2021-04-05 11:25:20 +0300

    v5.0

---
+++ src/main/java/Tar.java
@@ -0,0 +1,47 @@
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Tar {
+    public Tar() {
+    }
+
+    public static void main(String[] argsqwertyu) throws IOException {
+        String[] args = new String[]{"-u", "input/input.txt"};
+        if (args[0].equals("-u")) {
+            String a = readFile(args[1]);
+            String[] parts = a.split("---");
+            String[] var4 = parts;
+            int var5 = parts.length;
+
+            for(int var6 = 0; var6 < var5; ++var6) {
+                String part = var4[var6];
+                System.out.print("!");
+                System.out.print(part);
+            }
+        } else {
+            System.out.println(0);
+        }
+
+    }
+
+    public static String readFile(String file) {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            StringBuilder stringBuilder = new StringBuilder();
+            String ls = System.getProperty("line.separator");
+
+            String line;
+            while((line = reader.readLine()) != null) {
+                stringBuilder.append(line);
+                stringBuilder.append(ls);
+            }
+
+            stringBuilder.deleteCharAt(stringBuilder.length() - 1);
+            return stringBuilder.toString();
+        } catch (Exception var5) {
+            return null;
+        }
+    }
+}
